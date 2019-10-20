aad57e7d3db19aba898792dc4b8f1e2aa3953c92
thanhduong126 <hoangthanhduong09021995@gmail.com>
2019-10-21 01:22:27 +0700

    add file 21/10/2019

--- src/MangVaPhuongThucTrongJava/GopMang.java
+++ src/MangVaPhuongThucTrongJava/GopMang.java
@@ -1,34 +1,41 @@
 package MangVaPhuongThucTrongJava;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GopMang {
 	public static void main(String[] args) {
-		//Tạo 2 mảng cho trước
-		int[] arr = {1, 2, 3, 4, 5};
-		int[] arr1 = {2, 3, 4, 5, 6, 7};
-		int count1 = 0, count2 = 0;
-
-		//Sử dụng vòng lặp để duyệt phần tử mảng 1 va 2 đê đếm index cộng lại của 2 mảng
-		for (int i = 0; i < arr.length; i++) {
-			count1++;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a size of the first Array: ");
+		int sizeOfFirstArray = scanner.nextInt();
+		System.out.print("Enter the first Array: ");
+		int[] firstArray = new int[sizeOfFirstArray];
+		for (int i = 0; i < firstArray.length; i++) {
+			firstArray[i] = scanner.nextInt();
 		}
-		for (int i = 0; i < arr1.length; i++) {
-			count2++;
+		System.out.print("Enter a size of second array: ");
+		int sizeOfSecondArray = scanner.nextInt();
+		int[] secondArray = new int[sizeOfSecondArray];
+		for (int i = 0; i < secondArray.length; i++) {
+			secondArray[i] = scanner.nextInt();
 		}
-		int sum= count1+count2;
-		System.out.println("Số lượng phần tử trong mảng: "+sum);
-		//tạo mảng thứ 3 có kích thước bằng 2 mảng đã cho trước cộng lại
-		int[] arr2 = new int[sum];
-		//chạy vòng lặp cho mảng 1 gán cho chỉ số tương ứng với mảng 3
-		for (int i = 0; i < arr2.length; i++) {
-			arr2[i]=arr[i];
-			//Khi mà mảng 1 duyệt hết tiếp tục gán giá trị tiếp theo bằng với mảng 2
-			// độ dài mảng 3 sẽ trừ đi mảng 1 để lấy vị trí gán tiếp giá trị 0 của mảng 2 cho mảng 3
-			}
+		GopMang gopMang = new GopMang();
+		gopMang.poolingArray(firstArray, secondArray);
 
+	}
 
-
-
+	public void poolingArray(int[] first, int[] second) {
+		int sizeOfArray = first.length + second.length;
+		int[] theThirdArray = new int[sizeOfArray];
+		for (int i = 0; i < first.length; i++) {
+			theThirdArray[i] = first[i];
+		}
+		for (int i = 0; i < second.length; i++) {
+			theThirdArray[i + first.length] = second[i];
+		}
+		System.out.print("Array after merging is: ");
+		for (int i = 0; i < theThirdArray.length; i++) {
+			System.out.print(theThirdArray[i] + " ");
+		}
 	}
 }
