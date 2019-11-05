9982c3a88e83fab616184ee5ffb22f5ca359ee1d
thanhduong126 <hoangthanhduong09021995@gmail.com>
2019-11-05 12:19:25 +0700

    add file Database Furama 05/11/2019 task 8

---
+++ src/FuramaDatabase/TruyVanDuLieu.kt
@@ -0,0 +1,15 @@
+package FuramaDatabase
+
+-- Câu 2:
+Select * from nhanvien
+where ((HoTen like "H%") or (HoTen like "T%") or (HoTen like "K%")) and length(hoten)<=15;
+-- Câu 3:
+Select * from KhachHang
+where (age >=18 and age<=50) and (diachi="Đà Nẵng" or diachi="Quảng Trị");
+-- Câu4:
+Select hoten,tenloaikhach, count(hopdong.idkhachhang) as 'solandat' from khachhang
+inner join hopdong on khachhang.idkhachhang = hopdong.IDkhachhang
+inner join loaikhach on khachhang.IDLoaiKhach=loaikhach.IDLoaiKhach
+where TenLoaiKhach="Diamond"
+group by hoten
+order by solandat asc;
