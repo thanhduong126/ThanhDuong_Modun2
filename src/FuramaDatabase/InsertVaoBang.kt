215181aa716529a1f683631ed47a4edf36209451
thanhduong126 <hoangthanhduong09021995@gmail.com>
2019-11-05 10:27:24 +0700

    add file Database Furama 05/11/2019 task 8

--- src/FuramaDatabase/InsertVaoBang.kt
+++ src/FuramaDatabase/InsertVaoBang.kt
@@ -6,14 +6,14 @@ insert into BoPhan(TenBoPhan) values ("MarketTing"), ("Sale"),("Hành Chính"),(
 insert into KieuThue(TenKieuThue,gia) values ("Vip",300000),("Nomal",200000);
 insert into LoaiDichVu(TenLoaiDichVu) values ("Vip"),("Nomal"),("Hot");
 insert into DichVuDikem(TenDichVuDiKem,Gia) values ("Karaoke",150000),("Food",200000),("Car",120000),("Drink",180000);
-insert into LoaiKhach(TenLoaiKhach) values ("Cao cấp"),("Trung Cấp"),("Bình Thường");
+insert into LoaiKhach(TenLoaiKhach) values ("Diamond"),("Platium"),("Gold");
 
-insert into KhachHang(IDLoaiKhach,Hoten,NgaySinh,SoCMND,SDT,Email,DiaChi)
-values (1,"Hoàng","1994/10/10",32423423,2321432,"hoang@gmail.com","Quảng Trị"),
-(3,"Tiên","1995/10/10",3124234,2342342,"tien@gmail.com","Đà Nẵng"),
-(2,"Kiên","1992/06/04",2345345,324234,"kien@gmail.com","Huế"),
-(1,"Dương","1995/09/02",32423423,324234,"duong@gmail.com", "Huế"),
-(3,"Thanh","1993/05/02",324233443,432434,"thanh@gmail.com", "Đà Nẵng");
+insert into KhachHang(IDLoaiKhach,Hoten,NgaySinh,SoCMND,SDT,Email,DiaChi,age)
+values (1,"Hoàng","1994/10/10",32423423,2321432,"hoang@gmail.com","Quảng Trị",42),
+(3,"Tiên","1995/10/10",3124234,2342342,"tien@gmail.com","Đà Nẵng",20),
+(2,"Kiên","1992/06/04",2345345,324234,"kien@gmail.com","Huế",30),
+(1,"Dương","1995/09/02",32423423,324234,"duong@gmail.com", "Huế",18),
+(3,"Thanh","1993/05/02",324233443,432434,"thanh@gmail.com", "Đà Nẵng",51);
 
 insert into dichvu(TenDichVu,DienDich,SoTang,SoNguoiToiDa,ChiPhiThue,IDKieuThue,IDLoaiDichVu,TrangThai)
 values ("Villa",30,4,10,300000,1,2,"Vẫn còn"),
@@ -29,19 +29,21 @@ values ("Thiên",1,2,2,"1994/08/06",3245234,6000000,12312312,"thien@gmail.com","
 ("Jaya",5,1,3,"1997/01/02",4534234,8500000,56455342,"jaya@gmail.com","Quảng Bình");
 
 insert into HopDong(IDNhanVien,IDKhachHang,IDDichVu,NgayLamHopDong,NgayKetThuc,TienDatCoc,TongTien)
-values (7,6,4,"2002/01/01","2006/02/06",6000000,60000000),
-(12,10,5,"2010/08/09","2018/04/04",4000000,70000000),
-(9,6,6,"2011/06/12","2019/01/08",8000000,90000000),
-(8,6,4,"2007/09/11","2016/04/09",5000000,65000000),
-(7,7,6,"2007/09/11","2017/01/09",5000000,65000000),
-(10,8,5,"2005/02/02","2018/10/09",7000000,55000000),
-(11,9,4,"2004/06/06","2019/10/09",6000000,75000000);
+values (1,5,2,"2002/01/01","2006/02/06",600000,6000000),
+(2,3,1,"2010/02/09","2018/04/04",400000,7000000),
+(3,2,3,"2018/02/12","2019/01/08",800000,9000000),
+(4,4,2,"2018/09/11","2016/04/09",500000,6500000),
+(5,1,2,"2019/04/11","2019/09/09",600000,5500000),
+(6,5,1,"2019/09/11","2019/10/15",500000,6500000),
+(4,4,3,"2018/02/02","2018/10/09",700000,5500000),
+(3,3,2,"2019/06/06","2019/10/09",600000,7500000);
 
 insert into HopDongChiTiet(IDHopDong,IDDichVuDiKem,soluong)
-values (8,4,5),
-(14,1,1),
-(13,2,4),
-(12,3,3),
-(11,4,2),
-(9,3,1),
-(10,2,5);
+values (17,4,5),
+(18,1,1),
+(20,2,4),
+(19,3,3),
+(21,4,2),
+(24,3,1),
+(17,2,5);
+
